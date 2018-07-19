afb96db2609d612c76947e3f8408c84db6b6bd40
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-07-19 12:01:54 -0400

    Julio is dum so I had to comment every line
    Lol

--- JulioServo/JulioServo.ino
+++ JulioServo/JulioServo.ino
@@ -1,18 +1,18 @@
-#include <Servo.h>
+#include <Servo.h> //Include the library for using servos
 
-Servo julioServo; //Create new servo device
+Servo julioServo; //Create new servo device with the name julioServo
 
 
-void setup() {
+void setup() { //In the void setup thos runs once
   julioServo.attach(9); //Attach that servo device to pin 9
   
 
-}
+} //Always close void
 
-void loop() {
+void loop() { //The loop void runs over and over
   julioServo.write(0); //send the servo a value of 0
   delay(3000); //wait 3000 miliseconds
   julioServo.write(120); //send it 120
   delay(3000); //wait again
 
-}
+} //Close
