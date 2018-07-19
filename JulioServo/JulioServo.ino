a87c3d3257a89c842969ee720c4cc640aac24b1a
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-07-19 11:59:08 -0400

    Added comments
    Addded all the commeeeeeents

--- JulioServo/JulioServo.ino
+++ JulioServo/JulioServo.ino
@@ -1,18 +1,18 @@
 #include <Servo.h>
 
-Servo julioServo;
+Servo julioServo; //Create new servo device
 
 
 void setup() {
-  julioServo.attach(9);
+  julioServo.attach(9); //Attach that servo device to pin 9
   
 
 }
 
 void loop() {
-  julioServo.write(0);
-  delay(3000);
-  julioServo.write(120);
-  delay(3000);
+  julioServo.write(0); //send the servo a value of 0
+  delay(3000); //wait 3000 miliseconds
+  julioServo.write(120); //send it 120
+  delay(3000); //wait again
 
 }
